145bc3d54b7e25051e6e080a9002a6d360282352
mikematt33 <mdlewis2169@gmail.com>
2024-10-14 22:15:22 -0400

    adding the lego part component

--- src/components/lego-part/LegoPart.jsx
+++ src/components/lego-part/LegoPart.jsx
@@ -3,18 +3,39 @@ import React, { useEffect, useState } from "react";
 import "./lego-part.css";
 
 const LegoPart = () => {
+  const colors = ["red", "blue", "green", "yellow", "black", "white"]; // Add / Remove colors as needed
+
   const [partId, setPartId] = useState(null);
   const [partTitle, setPartTitle] = useState(null);
+  const [color, setColor] = useState("");
+  const [condition, setCondition] = useState("");
+  const [quantity, setQuantity] = useState(0);
 
   useEffect(() => {
-    setPartId(12345678);
-    setPartTitle("Lego Module Title Example ");
+    setPartId(); // Add part ID here
+    setPartTitle(); // Add part title here
   }, []);
+
+  const handleQuantityChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    setQuantity(value < 0 ? 0 : value);
+  };
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const handleConditionChange = (event) => {
+    setCondition(event.target.value);
+  };
+
+  // Check if all inputs are valid
+  const isFormValid = quantity > 0 && color !== "" && condition !== "";
 
   return (
     <div className="lego-part-background">
       <img
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F347394.jpg&f=1&nofb=1&ipt=53af06f774be36b82ea2ec0fa23ff03f89bccf28e3192fc032405e913d905fee&ipo=images"
+        src="" // Add image source here
         alt="lego img"
         className="lego-part-img"
       />
@@ -22,15 +43,58 @@ const LegoPart = () => {
       <p className="lego-part-title">{partTitle}</p>
       <div className="lego-part-vars-background">
         <div className="lego-part-var">
-          <label for="lego-quantity">Quantity: </label>
+          <label htmlFor="lego-quantity">Quantity: </label>
           <input
             className="lego-part-input-box"
             id="lego-quantity"
-            type="text"
+            type="number"
+            min="0"
+            value={quantity}
+            onInput={handleQuantityChange}
+            placeholder="⸻"
           />
         </div>
+        <div className="lego-part-var flex-container">
+          <label htmlFor="lego-color">Color: </label>
+          <select
+            id="lego-color"
+            value={color}
+            onChange={handleColorChange}
+            className="lego-part-select-box"
+          >
+            <option value="" disabled hidden>
+              ⸻
+            </option>
+            {colors.map((colorOption) => (
+              <option key={colorOption} value={colorOption}>
+                {colorOption.charAt(0).toUpperCase() + colorOption.slice(1)}{" "}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="lego-part-var flex-container">
+          <label htmlFor="lego-condition">Condition: </label>
+          <select
+            id="lego-condition"
+            value={condition}
+            onChange={handleConditionChange}
+            className="lego-part-select-box"
+          >
+            <option value="" disabled hidden>
+              ⸻
+            </option>
+            <option value="any">Any</option>
+            <option value="new">New</option>
+            <option value="used">Used</option>
+          </select>
+        </div>
       </div>
-      <button className="lego-part-button">Add to List</button>
+      <button
+        className={`lego-part-button ${isFormValid ? "valid" : ""}`}
+        disabled={!isFormValid}
+      >
+        Add to List
+      </button>
     </div>
   );
 };
